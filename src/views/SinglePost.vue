b7ec54953a6bea58feaa7e9f318c87d543a035ac
ATISHAY JAIN <atishayj2202@gmail.com>
2023-10-07 00:54:13 +0530

    Creating Single Post Show Page

---
+++ src/views/SinglePost.vue
@@ -0,0 +1,15 @@
+<script setup>
+export default {
+  name: 'PostTile',
+  data: function () {
+    return {
+      posts: [],
+      comments: []
+    }
+  }
+}
+</script>
+
+<template></template>
+
+<style scoped></style>
